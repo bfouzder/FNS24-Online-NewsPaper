ceb768e441ab94c6357ceeb3942e9cc3d4681b14
Bodhaditya Fouzder Aditya <bfouzder@yahoo.com>
2023-01-25 17:48:46 +0600

    Fixed the Speed issue for fns converter

--- banglaconverter/b2u.php
+++ banglaconverter/b2u.php
@@ -1,70 +1,73 @@
 <?php
 include_once('db.php');
+$output='';
+//$result = mysqli_query($con_db,"SELECT * FROM `right_word` where button='21'  ORDER BY `user_id` ASC  LIMIT 0 ,10000000000000000");
+$results = ar_select($con_db,"SELECT * FROM `right_word` where button='21'  ORDER BY `user_id` ASC  LIMIT 0 ,30000");
+require_once 'Unicode2Bijoy.php';
+$str=trim($_POST['convert']);
+$file = fopen("test5.txt","w");
+fwrite($file,"$str");
 
-require_once 'bijoy2unicode.php';
-$demo=trim($_POST['convert']);
+$myfile = fopen("test5.txt", "r") or die("Unable to open file!");
+//echo fread($myfile,filesize("test5.txt"));
 
-$value=$demo;
+$co=1;
+while ($line = fgets($myfile)) {
 
-
-$result = mysqli_query($con_db,"SELECT * FROM `right_word` where  button='1'  ORDER BY `user_id` ASC  LIMIT 0 ,10000000000000000");
-
-while ($a_row = mysqli_fetch_row($result)) {
-
-	foreach ($a_row as $field) {
-		
-	}
-	
-$value=str_replace("$a_row[1]","$a_row[2]","$value");
-	
-}    
-
-
-
-
-$demo=$value;
-
-$converted = convertBijoyToUnicode($demo);
-
-
-$data_final=$converted;
-
-
-$value=$data_final;
+//print"$line $co";
+$str=$line;
+$val=Unicode2Bijoy::convert($str);
+//$val=dbReplace($results, $val,$co);
+$output .=$val;
+//print"$val";
+ if($line=='')
+ {   
+ } 
+ $co++;
+} 
+fclose($myfile);
 
 
 
+$val = dbReplace($results, $output, $co=1);
+print"$val";
 
 
-$result = mysqli_query($con_db,"SELECT * FROM `right_word` where button='20'   ORDER BY `user_id` ASC  LIMIT 0 ,10000000000000000");
+################bof db correction##########################
+function dbReplace($results, $value, $co){
 
-while ($a_row = mysqli_fetch_row($result)) {
+   // echo '<br/>Line '.$co.'s = '.$value.'<hr/>';
+    //print_r($results);
+    foreach ($results as $a_row) {	
+      $value=str_replace($a_row['wrong'],$a_row['right_w'],$value);
+    }
 
-	foreach ($a_row as $field) {
-		
-	}
-	
-$value=str_replace("$a_row[1]","$a_row[2]","$value");
-	
-}    
+    return $value;
+}
+################eof db correction##########################
+function pre($value){ echo '<pre>'; print_r($value);echo '</pre>';}
 
-
-
-
-
-
-$data_final=$value;
-
-
-
-print"$data_final";
-
-
-
-
-
-//print"$converted";
-
-
-
+function ar_select ($conn,$sql)
+    {	    
+    
+      try { 
+        $results = mysqli_query($conn,$sql, MYSQLI_USE_RESULT); 
+      } catch (mysqli_sql_exception $e) { 
+      var_dump($e);
+      die("sqli_exception: ".$e );
+      return false;
+      }
+        if( (!$results) or (empty($results)) ) {
+              return false;
+      }
+      $count = 0;
+      $data = array();
+      while ( $row = mysqli_fetch_array($results, MYSQLI_ASSOC))
+      {
+              $data[$count] = $row;
+              $count++;
+      }
+      mysqli_free_result($results);
+    return $data;
+    }
 ?>
